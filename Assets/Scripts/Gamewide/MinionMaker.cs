cbc3369821964067acf8c5b3a7b2cce55ed7c393
jclayton128 <jclayton128@gmail.com>
2021-06-10 07:26:28 -0400

    MinionMaker auto-registers new minions if they haven't been spawned in the game so far. No need now to include on the network manager spawnable list.

--- Assets/Scripts/Gamewide/MinionMaker.cs
+++ Assets/Scripts/Gamewide/MinionMaker.cs
@@ -8,6 +8,7 @@ public class MinionMaker : NetworkBehaviour
 {
     PersNetworkManager pnm;
     ArenaBounds ab;
+    List<GameObject> registeredMinions = new List<GameObject>();
     public override void OnStartServer()
     {
         base.OnStartServer();
@@ -15,8 +16,21 @@ public class MinionMaker : NetworkBehaviour
         ab = FindObjectOfType<ArenaBounds>();
     }
 
+    public void RegisterMinionAcrossNetwork(GameObject minion)
+    {
+        // Minions should reside in each level. Just before any minions are spawned, they should be registered on all clients.
+        // Minion prefabs should not be hand-dropped into the network manager's list.
+
+        if (!registeredMinions.Contains(minion))
+        {
+            registeredMinions.Add(minion);
+            NetworkClient.RegisterPrefab(minion);
+        }
+    }
+
     public void SpawnNewMinion(GameObject chosenMinion)
     {
+        RegisterMinionAcrossNetwork(chosenMinion);
         Vector2 startPos = ab.CreateValidRandomPointWithinArena();
         GameObject newMinion = Instantiate(chosenMinion, startPos, Quaternion.identity) as GameObject;
         NetworkServer.Spawn(newMinion);

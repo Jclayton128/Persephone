7de2473d26527dce963680ec7c70bd7631780dda
jclayton128 <jclayton128@gmail.com>
2021-07-05 23:54:50 -0400

    Torpedo_AI. First stage of Reanimator Torpedo.

---
+++ Assets/Scripts/Abilities/Wreckromancer/Ability_ReanimatorTorpedo.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using System;
+
+public class Ability_ReanimatorTorpedo : Ability
+{
+    Ability_RockMode arm;
+    bool hasActiveTorpedo = false;
+    Torpedo_AI activeTorpedo;
+    [SerializeField] float weaponTurnRate;
+
+    protected override void Start()
+    {
+        base.Start();
+        arm = GetComponent<Ability_RockMode>();
+    }
+
+    protected override void MouseClickDownEffect()
+    {
+        if (arm.CheckFullyDeployed())
+        {
+            CmdRequestFireWeapon();
+            hasActiveTorpedo = true;
+            //TODO audio for client
+        }
+
+    }
+
+    private void CmdRequestFireWeapon()
+    {
+        if (es.CheckSpendEnergy(costToActivate) && arm.CheckFullyDeployed())
+        {
+            GameObject bullet = Instantiate(abilityPrefabs[0], am.SecondaryMuzzle.position, am.SecondaryMuzzle.transform.rotation) as GameObject;
+            NetworkServer.Spawn(bullet);
+            activeTorpedo = bullet.GetComponent<Torpedo_AI>();
+            activeTorpedo.normalSpeed = weaponSpeed;
+            activeTorpedo.maxTurnRate = weaponTurnRate;
+            bullet.layer = 9;
+            bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.up * weaponSpeed;
+
+            bullet.GetComponent<Torpedo_AI>().SetTargetPoint(MouseHelper.GetMouseCursorLocation());
+
+            DamageDealer dd = bullet.GetComponent<DamageDealer>();
+            dd.SetNormalDamage(normalDamage);
+            Destroy(bullet, weaponLifetime);
+        }
+    }
+
+    protected override void MouseClickUpEffect()
+    {
+        if (hasActiveTorpedo)
+        {
+            hasActiveTorpedo = false;
+            CmdDetonateTorpedo();
+        }
+    }
+
+    [Command]
+    private void CmdDetonateTorpedo()
+    {
+        activeTorpedo.Detonate();
+    }
+
+    private void CmdPassTargetPoint(Vector2 vector2)
+    {
+        activeTorpedo.SetTargetPoint(vector2);
+    }
+
+    private void Update()
+    {
+        if (isClient && hasActiveTorpedo)
+        {
+            CmdPassTargetPoint(MouseHelper.GetMouseCursorLocation());
+        }
+    }
+}
---
+++ Assets/Torpedo_AI.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Torpedo_AI : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] GameObject payloadPrefab = null;
+
+    Vector3 targetPoint;
+    public float normalSpeed;
+    public float maxTurnRate;
+    float thrustTurning = 90f;
+    float closeEnough = 1.0f;
+
+    float distToDest;
+    float angleToDest;
+
+    private void Awake()
+    {
+        if (!NetworkClient.prefabs.ContainsValue(payloadPrefab))
+        {
+            NetworkClient.RegisterPrefab(payloadPrefab);
+        }
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetTargetPoint(Vector2 newTargetPoint)
+    {
+        targetPoint = newTargetPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateNavData();
+        SteerTowardsDestination();
+        rb.velocity = transform.up * normalSpeed;
+
+        HandleReachingDestination();
+
+    }
+
+    private void UpdateNavData()
+    {
+        Vector3 dir = targetPoint - transform.position;
+        distToDest = dir.magnitude;
+        angleToDest = Vector3.SignedAngle(dir, transform.up, transform.forward);
+    }
+    private void SteerTowardsDestination()
+    {
+        if (angleToDest > 1)
+        {
+            rb.angularVelocity = Mathf.Lerp(rb.angularVelocity, -maxTurnRate, thrustTurning * Time.deltaTime);
+        }
+        if (angleToDest < -1)
+        {
+            rb.angularVelocity = Mathf.Lerp(rb.angularVelocity, maxTurnRate, thrustTurning * Time.deltaTime);
+        }
+    }
+    private void HandleReachingDestination()
+    {
+        if (distToDest < closeEnough)
+        {
+            Detonate();
+        }
+    }
+
+    public void Detonate()
+    {
+        if (payloadPrefab)
+        {
+            GameObject payload = Instantiate(payloadPrefab, transform.position, transform.rotation) as GameObject;
+            NetworkServer.Spawn(payload);
+        }
+        Destroy(gameObject);
+    }
+
+
+}
